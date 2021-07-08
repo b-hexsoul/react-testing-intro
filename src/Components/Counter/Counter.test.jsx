18eda174c06b0eae5e332114b7dd90944e05e472
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-08 02:04:23 -0700

    'Adding more tests to the Counter Component'

--- src/Components/Counter/Counter.test.jsx
+++ src/Components/Counter/Counter.test.jsx
@@ -8,3 +8,21 @@ test('The h1 should renders with correct text', () => {
   const headerEl = component.getByTestId('header');
   expect(headerEl.textContent).toBe('Counter');
 });
+
+test('The count text should initialize with 0', () => {
+  const component = render(<Counter />);
+  const countEl = component.getByTestId('count');
+  expect(countEl.textContent).toBe(0);
+});
+
+test('The minus button should have correct text', () => {
+  const component = render(<Counter />);
+  const minusButton = component.getByTestId('minus-button');
+  expect(minusButton.textContent).toBe('-');
+});
+
+test('The add button should have correct text', () => {
+  const component = render(<Counter />);
+  const addButton = component.getByTestId('add-button');
+  expect(addButton.textContent).toBe('+');
+});
