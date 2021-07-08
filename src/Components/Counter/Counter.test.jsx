b65d2fab7505cda7ad37bfdf5e8455ebf7f27369
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-08 02:52:47 -0700

    'Adding more tests'

--- src/Components/Counter/Counter.test.jsx
+++ src/Components/Counter/Counter.test.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import Counter from './Counter';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-test('The h1 should renders with correct text', () => {
+test('The header should renders with correct text', () => {
   const component = render(<Counter />);
   const headerEl = component.getByTestId('header');
   expect(headerEl.textContent).toBe('Counter');
 });
 
-test('The count text should initialize with 0', () => {
+test('counter initially start with text of 0', () => {
   const component = render(<Counter />);
-  const countEl = component.getByTestId('count');
-  expect(countEl.textContent).toBe(0);
+  const countEl = component.getByTestId('counter');
+  expect(countEl.textContent).toBe('0');
 });
 
-test('The minus button should have correct text', () => {
+test('The input has initial value of 1', () => {
+  const component = render(<Counter />);
+  const inputEl = component.getByTestId('input');
+  expect(inputEl.value).toBe('1');
+});
+
+test('The minus button renders with -', () => {
   const component = render(<Counter />);
   const minusButton = component.getByTestId('minus-button');
   expect(minusButton.textContent).toBe('-');
 });
 
-test('The add button should have correct text', () => {
+test('The add button renders with +', () => {
   const component = render(<Counter />);
   const addButton = component.getByTestId('add-button');
   expect(addButton.textContent).toBe('+');
